b0b033f6cd04e18eba157d7ad29a4e38b872ac67
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-06-19 10:58:19 +0200

    TensorBoard support added

--- deepnet.py
+++ deepnet.py
@@ -1,9 +1,22 @@
 import numpy as np
 import h5py
+from time import time
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
+from keras.callbacks import TensorBoard
+from keras import backend as K
+
+# Keras settings
+K.set_image_dim_ordering('th')
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+						  histogram_freq=10,
+						  batch_size=10, write_graph=True,
+						  write_grads=True, write_images=True,
+						  embeddings_freq=0, embeddings_layer_names=None,
+						  embeddings_metadata=None, embeddings_data=None)
 
 def readData():
 	'''
@@ -26,10 +39,6 @@ readData()
 
 X_train = X_train.reshape(42000,28,28)
 X_test = X_test.reshape(28000,28,28)
-
-# X_train = X_train[:1000,:,:]
-# X_test = X_test[:1000,:,:]
-# Y_train = Y_train[:1000]
 
 # Random number seed for reproducibility
 np.random.seed(123)
@@ -62,10 +71,12 @@ model.add(Dense(10, activation='softmax'))
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+print(model.summary())
 
 # Fit model on training data
 model.fit(X_train, Y_train,
-          batch_size=32, epochs=10, verbose=1)
+          batch_size=32, epochs=250, verbose=1,
+          callbacks=[tensorboard])
 
 # Evaluate model on test data
 # score = model.evaluate(X_test, Y_test, verbose=0)
